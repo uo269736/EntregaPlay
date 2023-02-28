97976a96b56f3c43e118bc3c4a288c9860ed1c5d
uo269736 <uo269736@uniovi.es>
2023-02-28 20:54:13 +0100

    Añadidos test funcionales (Ana y Miguel)

--- entregaplay_recetas/test/controllers/HomeControllerTest.java
+++ entregaplay_recetas/test/controllers/HomeControllerTest.java
@@ -1,18 +1,24 @@
 package controllers;
 
-import org.apache.http.client.methods.RequestBuilder;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import models.ImagenReceta;
+import models.Ingrediente;
+import models.Propiedad;
+import models.Receta;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import play.Application;
-import play.inject.guice.GuiceApplicationBuilder;
+import play.libs.Json;
 import play.mvc.Http;
 import play.mvc.Result;
 import play.test.Helpers;
 import play.test.WithApplication;
+import play.twirl.api.Content;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 import static play.mvc.Http.Status.OK;
-import static play.test.Helpers.GET;
-import static play.test.Helpers.route;
+import static play.test.Helpers.*;
 
 public class HomeControllerTest extends WithApplication {
 
@@ -31,24 +37,25 @@ public class HomeControllerTest extends WithApplication {
         assertEquals(200, r.status());
     }
 
-//    @Test
-//    public void testIndex() {
-//        Http.RequestBuilder request = new Http.RequestBuilder()
-//                .method(GET)
-//                .uri("/");
-//
-//        Result result = route(app, request);
-//        assertEquals(OK, result.status());
-//    }
-//
-//    @Test
-//    public void testIndex() {
-//        Http.RequestBuilder request = new Http.RequestBuilder()
-//                .method(GET)
-//                .uri("/");
-//
-//        Result result = route(app, request);
-//        assertEquals(OK, result.status());
-//    }
+    @Test
+    public void testGetRecetaNoexiste() {
+        Http.RequestBuilder req = Helpers.fakeRequest()
+                .method("GET")
+                .uri("/receta/100");
+
+        Result r = Helpers.route(app, req);
+        assertEquals(404, r.status());
+    }
+
+    @Test
+    public void testRecetaView() {
+        Receta r = new Receta();
+        r.addIngredientes(new Ingrediente());
+        r.addPropiedad(new Propiedad());
+        r.setImagen(new ImagenReceta());
+        Content res = views.xml.receta.render(r);
+        assertEquals("application/xml", res.contentType());
+        assertTrue(res.body().contains("<receta>"));
+    }
 
 }
